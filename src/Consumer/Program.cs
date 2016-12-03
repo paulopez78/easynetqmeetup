fd13316cf71a452d6e897f3a32c5f1fbaf902b07
paulopez78 <paulopez78@gmail.com>
2016-12-03 09:58:40 +0100

    create publisher and consumer host projects

--- src/Consumer/Program.cs
+++ src/Consumer/Program.cs
@@ -1,33 +1,21 @@
 ﻿using System;
-using System.Linq;
 using EasyNetQ;
-using EasyQMeetup.Domain;
 using EasyQMeetup.Domain.Events;
 
 namespace EasyQMeetup
 {
-    public class Program
+    public class Consumer
     {
         public static void Main(string[] args)
         {
             var bus = RabbitHutch.CreateBus("amqp://guest:guest@localhost:5672");
 
-            var easyMeetup = new Meetup(1, "EasyNetQ Meetup Demo");
-            easyMeetup.RSVP("Pau", 2);
-            easyMeetup.CancelRSVP("Pau");
-    
             bus.Subscribe<RSVPConfirmedEvent>("MeetupRSVP_Subscription", 
                 @event => Console.WriteLine($"RSVP confirmed for {@event.UserName} with {@event.PlusNumber}"));
 
             bus.Subscribe<RSVPCancelledEvent>("MeetupRSVP_Subscription", 
                 @event => Console.WriteLine($"RSVP cancelled for {@event.UserName}"));
 
-            Console.WriteLine("Publishing events.");
-            
-            foreach (dynamic domainEvent in easyMeetup.GetEvents())
-                bus.Publish(domainEvent);
-
-            Console.WriteLine("Events published.");
             Console.ReadLine();
         }
     }
---
+++ src/Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using EasyNetQ;
+using EasyQMeetup.Domain;
+
+namespace EasyQMeetup
+{
+    public class Publisher
+    {
+        public static void Main(string[] args)
+        {
+            var bus = RabbitHutch.CreateBus("amqp://guest:guest@localhost:5672");
+
+            var easyMeetup = new Meetup(1, "EasyNetQ Meetup Demo");
+            easyMeetup.RSVP("Pau", 2);
+            easyMeetup.CancelRSVP("Pau");
+    
+            Console.WriteLine("Publishing events.");
+            
+            foreach (dynamic domainEvent in easyMeetup.GetEvents())
+                bus.Publish(domainEvent);
+
+            Console.WriteLine("Events published.");
+            Console.ReadLine();
+        }
+    }
+}
